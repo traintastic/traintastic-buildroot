8bc7767bfd457b4f5e5ce6d3917ac393c54835e7
Reinder Feenstra <reinderfeenstra@gmail.com>
2024-03-03 14:49:36 +0100

    traintastic-server now runs as traintastic-server user

--- traintastic/package/traintastic-server/traintastic.mk
+++ traintastic/package/traintastic-server/traintastic.mk
@@ -13,8 +13,16 @@ TRAINTASTIC_SERVER_SUBDIR = server
 TRAINTASTIC_SERVER_INSTALL_STAGING = NO
 TRAINTASTIC_SERVER_INSTALL_TARGET = YES
 
-define TRAINTASTIC_INSTALL_INIT_SYSV
+define TRAINTASTIC_SERVER_INSTALL_INIT_SYSV
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_TRAINTASTIC_PATH)/package/traintastic-server/S90traintastic-server $(TARGET_DIR)/etc/init.d/S90traintastic-server
+endef
+
+define TRAINTASTIC_SERVER_USERS
+	traintastic-server -1 traintastic -1 * - - - Traintastic-server daemon
+endef
+
+define TRAINTASTIC_SERVER_PERMISSIONS
+	/var/opt/traintastic d 0755 traintastic-server traintastic - - - - -
 endef
 
 $(eval $(cmake-package))
